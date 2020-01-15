71ec5efeb6b7e6dd4afee9b73b2a55324c907c6a
Jakub Biskup <elldiablo7@gmail.com>
2020-01-15 23:40:04 +0100

    Creating first model

--- DjangoBlog/Blog/views.py
+++ DjangoBlog/Blog/views.py
@@ -1,27 +1,11 @@
 from django.shortcuts import render
-from . import urls
-
-
-posts = [
-    {
-        'author': 'Jakub Biskup',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 13, 2020'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 14, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts,
-        'urls': urls.urlmapping}
+        'posts': Post.objects.all()
+    }
     return render(request, 'blog/home.html', context)
 
 
